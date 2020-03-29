9c3e64190add14e02bbbbb98f26a756bf464438e
gmsantos <gabriel.ms1@hotmail.com>
2020-03-29 19:26:43 -0300

    fix s3 client region

--- src/main/java/br/com/treinaweb/s3upload/FileUploadController.java
+++ src/main/java/br/com/treinaweb/s3upload/FileUploadController.java
@@ -1,11 +1,21 @@
 package br.com.treinaweb.s3upload;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import com.amazonaws.services.s3.transfer.model.UploadResult;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -19,21 +29,41 @@ public class FileUploadController {
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
+	@Value("${cloud.aws.region.static}")
+	private String region;
+
+	private AmazonS3 s3Client;
+
+	private TransferManager transferManager;
+
+	@PostConstruct
+	public void init() {
+		s3Client = AmazonS3ClientBuilder.standard().withRegion(region).build();
+		transferManager = TransferManagerBuilder.standard().withS3Client(s3Client).build();
+	}
+
 	@PostMapping("/upload")
-	public String uploadFile(@RequestPart(value = "file") final MultipartFile file) {
+	public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
 		try {
 			ObjectMetadata objectMetadata = new ObjectMetadata();
 			objectMetadata.setContentType(file.getContentType());
 
-			TransferManager transferManager = TransferManagerBuilder.defaultTransferManager();
 			UploadResult result = transferManager.upload(
-				bucket, "uploads/" + file.getName(), 
+				bucket, "uploads/" + file.getOriginalFilename(), 
 				file.getInputStream(), objectMetadata
 			).waitForUploadResult();
 
 			return result.getKey();
 		} catch (Exception e) {
-			return "error " + e.getStackTrace();
+			e.printStackTrace();
+			return "error ";
 		}
+	}
+
+	@GetMapping("/file/{key}")
+	public String getFile(@PathVariable("key") String key) {
+		Date inTwoHours = Date.from(Instant.now().plus(2, ChronoUnit.HOURS));
+
+		return s3Client.generatePresignedUrl(bucket, key, inTwoHours).toString();
 	}
 }
