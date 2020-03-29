062729e929af59b4b82be6b507d86c58640353dc
gmsantos <gabriel.ms1@hotmail.com>
2020-03-29 12:13:45 -0300

    Initial commit

---
+++ src/test/java/br/com/treinaweb/s3upload/S3uploadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.s3upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3uploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
